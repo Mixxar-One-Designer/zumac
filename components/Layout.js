a7a9ff0922e26434bec059ed46e1f7ad9f0554ff
Mixxar-One-Designer <muazzammixxa32@gmail.com>
2021-08-23 03:33:02 +0100

    Create order confirmation screen

---
+++ components/Layout.js
@@ -0,0 +1,109 @@
+import React, { useContext, useEffect } from "react";
+import { 
+    ThemeProvider, 
+    CssBaseline, 
+    AppBar, 
+    Toolbar,
+    Link, 
+    Container,
+    Typography,
+    Box,
+    CircularProgress,
+    Badge,
+ } from "@material-ui/core";
+import { theme, useStyles } from "../utils/styles";
+import Head from "next/head";
+import NextLink from 'next/link';
+import getCommerce from "../utils/commerce";
+import { CART_RETRIEVE_REQUEST, CART_RETRIEVE_SUCCES } from "../utils/constants";
+import { Store } from "./Store";
+
+
+export default function Layout({
+    children,
+    commercePublicKey,
+    title = 'Zuma',
+}) {
+    const classes = useStyles();
+    const { state, dispatch } = useContext(Store);
+    const { cart } = state;
+
+    useEffect(() => {
+        const fetchCart = async () => {
+            const commerce = getCommerce(commercePublicKey);
+            dispatch({ type: CART_RETRIEVE_REQUEST });
+            const cartData = await
+            commerce.cart.retrieve();
+            dispatch({ type: CART_RETRIEVE_SUCCES, payload: cartData });
+        };
+        fetchCart();
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Head>
+                <meta charset='utf-8' />
+                <title>{`${title} - Zuma `}</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta 
+                name="viewport" 
+                content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppBar 
+                position="static"
+                color="default"
+                elevation={0}
+                className={classes.appBar}
+                >
+                    <Toolbar className={classes.toolbar}>
+                        <NextLink href="/">
+                            <Link 
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            href="/"
+                            className={classes.toolbarTitle}
+                            >
+                                Zuma
+                            </Link>
+                        </NextLink>
+                        <nav>
+                            <NextLink href="/cart">
+                                <Link 
+                                variant="button"
+                                color="textPrimary"
+                                href="/cart"
+                                className={classes.link}
+                                >
+                                    {cart.loading ? ( 
+                                        <CircularProgress />
+                                    ) : cart.data.total_items > 0 ? (
+                                        <Badge badgeContent={cart.data.total_items} color="primary">
+                                            Cart
+                                        </Badge>
+                                    ) : (
+                                        'Cart'
+                                    )}
+                                </Link>
+                            </NextLink>
+                        </nav>
+                    </Toolbar>
+                </AppBar>
+                <Container component="main" className={classes.main}>
+                    {children}
+                </Container>
+                <Container maxWidth="md" component="footer">
+                <Box mt={5}>
+                    <Typography variant="body2" color="default" align="center">
+                        &copy; Copyright Zuma.com, inc. 2021 All right reserved
+                        {'.'}
+                    </Typography>
+                </Box>
+                </Container>
+            </ThemeProvider>
+        </React.Fragment>
+    );
+};
---
+++ pages/products/[id].js
@@ -0,0 +1,129 @@
+import { 
+    Box, 
+    Button, 
+    Card,  
+    Grid, 
+    Link, 
+    List, 
+    ListItem, 
+    Slide, 
+    Typography
+} from '@material-ui/core';
+import { useContext, useState } from 'react';
+import Layout from '../../components/Layout';
+import { Store } from '../../components/Store';
+import getCommerce from '../../utils/commerce';
+import { CART_RETRIEVE_SUCCES } from '../../utils/constants';
+import { useStyles } from '../../utils/styles';
+import Router from 'next/router';
+import NextLink from 'next/link';
+  
+  export default function Product(props) {
+    const { product } = props;
+    const [quantity, setQuantity] = useState(1);
+
+    const classes = useStyles();
+
+    const { state, dispatch } = useContext(Store);
+    const { cart } = state;
+
+    const addToCartHandler = async () => {
+        const commerce = getCommerce(props.commercePublicKey);
+        const lineItem = cart.data.line_items.find(
+            (x) => x.product_id === product.id
+        );
+        if (lineItem) {
+            const cartData = await commerce.cart.update(lineItem.id);
+            dispatch({ type: CART_RETRIEVE_SUCCES, payload: cartData.cart });
+            Router.push('/cart');
+        } else {
+            const cartData = await commerce.cart.add(product.id);
+            dispatch({ type: CART_RETRIEVE_SUCCES, payload: cartData.cart });
+            Router.push('/cart');
+        };
+    };
+
+    return (
+      <Layout title={product.name} commercePublicKey={props.commercePublicKey}>
+          <div className={classes.section}>
+              <NextLink href="/" passHref>
+                  <Link>
+                  <Typography> Back to store </Typography>
+                  </Link>
+              </NextLink>
+          </div>
+          <Slide direction="up" in={true}>
+              <Grid container spacing={1}>
+                  <Grid item md={6}>
+                      <img 
+                      src={product.media.source}
+                      alt={product.name}
+                      className={classes.largeImage} 
+                      />
+                  </Grid>
+                  <Grid item md={3} xs={12}>
+                      <List>
+                          <ListItem>
+                              <Typography 
+                              gutterBottom
+                              variant="h6"
+                              color="textPrimary"
+                              component="h1"
+                              >
+                                {product.name}
+                              </Typography>
+                          </ListItem>
+                          <ListItem>
+                              <Box 
+                              dangerouslySetInnerHTML={{ __html: product.description }}
+                              ></Box>
+                          </ListItem>
+                      </List>
+                  </Grid>
+                  <Grid item md={3} xs={12}>
+                      <Card>
+                          <List>
+                              <ListItem>
+                                  <Grid container>
+                                      <Grid item xs={6}>
+                                          Price
+                                      </Grid>
+                                      <Grid item xs={6}>
+                                          {product.price.formatted_with_symbol}
+                                      </Grid>
+                                  </Grid>
+                              </ListItem>
+                                  <>
+                                  <ListItem>
+                                      <Button 
+                                      type="button"
+                                      fullWidth
+                                      variant="contained"
+                                      color="primary"
+                                      onClick={addToCartHandler}
+                                      >
+                                          Add to cart
+                                      </Button>
+                                  </ListItem>
+                                  </>
+                          </List>
+                      </Card>
+                  </Grid>
+              </Grid>
+          </Slide>
+      </Layout>
+    );
+  }
+  
+  export async function getServerSideProps({ params }) {
+    const { id } = params;
+    const commerce = getCommerce();
+    const product = await commerce.products.retrieve(id, {
+        type: 'permalink',
+    });
+    return {
+      props: {
+        product,
+      },
+    };
+  };
